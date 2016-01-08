8de57a1cb8392ed97464eab602d35d07a811bbfc
Leandro Franchi <leandro.franchi@gmail.com>
2016-01-08 11:34:28 -0200

    #1

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -32,25 +32,21 @@ INSERT INTO `eventz`.`data_field_metadata` (`name`,`field_order`,`type`,`produce
 ------------------------------------------------------------------------------------------------------------
 INSERT INTO `eventz`.`producer_group`(`name`) VALUES ('BOMBAS');
 
--- TODO INSERIR ON EXCEPTION AQUI
+-- On Exception deve ser logado.
+INSERT INTO `eventz`.`event_to_process`(`event`) VALUES (1);
+INSERT INTO `eventz`.`producer_group_events_on_exception`(`producer_group`,`events_on_exception`) VALUES (1,1);
 
--- TODO INSERIR ON ALAWAYS PARA SALVAR AQUI 
+-- Toda execução deve ser salva.
+INSERT INTO `eventz`.`event_to_process`(`event`) VALUES (2);
+INSERT INTO `eventz`.`producer_group_events_on_always`(`producer_group`,`events_on_always`)VALUES(1,2);
 ------------------------------------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------------------------------------
 -- Produtores
 ------------------------------------------------------------------------------------------------------------
 INSERT INTO `eventz`.`producer`(`name`, `metadata`,`producer_group`) VALUES ('B1', 1, 1);
-INSERT INTO `eventz`.`event_to_process`(`event`) VALUES (1);
-INSERT INTO `eventz`.`producer_events_on_exception`(`producer`,`events_on_exception`) VALUES (1,1);
-
 INSERT INTO `eventz`.`producer`(`name`, `metadata`,`producer_group`) VALUES ('B2', 1, 1);
-INSERT INTO `eventz`.`event_to_process`(`event`) VALUES (1);
-INSERT INTO `eventz`.`producer_events_on_exception`(`producer`,`events_on_exception`) VALUES (2,2);
-
 INSERT INTO `eventz`.`producer`(`name`, `metadata`,`producer_group`) VALUES ('B3', 1, 1);
-INSERT INTO `eventz`.`event_to_process`(`event`) VALUES (1);
-INSERT INTO `eventz`.`producer_events_on_exception`(`producer`,`events_on_exception`) VALUES (3,3);
 ------------------------------------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------------------------------------
@@ -67,14 +63,13 @@ INSERT INTO `eventz`.`alarm_level`(`color`,`image`,`name`) VALUES('red','alert.p
 -- Gerar alarme de warning quando conteudo estiver entre 2 e 4.
 INSERT INTO `eventz`.`rule`(`formula`,`name`,`type`,`producer`,`producer_group`) VALUES ('content > 2 and content <= 4','BOMBA - Conteudo em Atenção','JEXL',NULL,1);
 INSERT INTO `eventz`.`event_to_process`(`event`) VALUES (3);
-INSERT INTO `eventz`.`rule_events_on_true`(`rule`,`events_on_true`) VALUES (1, 4);
-INSERT INTO `eventz`.`event_to_process_properties`(`event_to_process`,`value`,`properties_key`) VALUES (4, '2', 1);
+INSERT INTO `eventz`.`rule_events_on_true`(`rule`,`events_on_true`) VALUES (1, 3);
+INSERT INTO `eventz`.`event_to_process_properties`(`event_to_process`,`value`,`properties_key`) VALUES (3, '2', 1);
 
 -- Gerar alarme de alerta quando conteudo for menor que 2.
 INSERT INTO `eventz`.`rule`(`formula`,`name`,`type`,`producer`,`producer_group`) VALUES ('content <= 2','BOMBA - Conteudo em Alerta','JEXL',NULL,1);
 INSERT INTO `eventz`.`event_to_process`(`event`) VALUES (3);
-INSERT INTO `eventz`.`rule_events_on_true`(`rule`,`events_on_true`) VALUES (1, 5);
-INSERT INTO `eventz`.`event_to_process_properties`(`event_to_process`,`value`,`properties_key`) VALUES (5, '3', 1);
+INSERT INTO `eventz`.`rule_events_on_true`(`rule`,`events_on_true`) VALUES (1, 4);
+INSERT INTO `eventz`.`event_to_process_properties`(`event_to_process`,`value`,`properties_key`) VALUES (4, '3', 1);
+------------------------------------------------------------------------------------------------------------
 
-
--- TODO: FINALIZAR.
