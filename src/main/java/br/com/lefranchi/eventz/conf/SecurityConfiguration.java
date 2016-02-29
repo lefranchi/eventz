43ae5b4dc358cf3d9eb0b2d78e4df5ebeba7df71
Administrator <leandro.franchi@flexvision.com.br>
2016-02-29 19:45:28 -0300

    Security Resources Enabled.

--- src/main/java/br/com/lefranchi/eventz/conf/SecurityConfiguration.java
+++ src/main/java/br/com/lefranchi/eventz/conf/SecurityConfiguration.java
@@ -1,18 +1,34 @@
 package br.com.lefranchi.eventz.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(final HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+		httpSecurity.authorizeRequests().antMatchers("/login").permitAll().anyRequest().fullyAuthenticated().and()
+				.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
 
 		httpSecurity.csrf().disable();
 		httpSecurity.headers().frameOptions().disable();
+	}
+
+	@Override
+	public void configure(final WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**").antMatchers("/images/**").antMatchers("/css/**");
+	}
+
+	@Autowired
+	public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("pwd").roles("USER");
 	}
 
 }
